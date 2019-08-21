b32b88e27343bd70b7746adf7db8284cf49b5a61
Isac <isac.junior@ifood.com.br>
2019-08-21 11:35:04 -0300

    add provider

---
+++ lib/src/containers/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../stores/counter/state.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _counter = Provider.of<CounterStore>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider counter page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer<CounterStore>(
+              builder: (context, counter, _) {
+                return Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () => _counter.dispatch(CounterTypes.decrement),
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(width: 10,),
+          FloatingActionButton(
+            onPressed: () => _counter.dispatch(CounterTypes.increment),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/stores/counter/state.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import './action.dart';
+
+enum CounterTypes {
+  increment,
+  decrement,
+}
+
+class CounterStore with ChangeNotifier {
+  Counter _counter = Counter(count: 0);
+
+  int get count => _counter.count;
+
+  void dispatch(CounterTypes type) {
+    switch(type) {
+      case CounterTypes.increment: _counter.increment();
+      break;
+      case CounterTypes.decrement: _counter.decrement();
+      break;
+    }
+    notifyListeners();
+  }
+}
